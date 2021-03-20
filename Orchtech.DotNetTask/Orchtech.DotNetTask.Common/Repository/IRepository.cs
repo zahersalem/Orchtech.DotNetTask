de7bd5b70c92a51f620370728781ce8df4d0bac7
Zaher Salem <zahersalem46@gmail.com>
2021-03-20 13:06:09 +0200

    Create: Common Project

---
+++ Orchtech.DotNetTask/Orchtech.DotNetTask.Common/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Orchtech.DotNetTask.Common.Repository
+{
+    public interface IRepository
+    {
+        Task<List<T>> GetAll<T>() where T : class;
+        Task<T> Find<T>(Guid id) where T : class;
+        Task CreateAsync<T>(T entity) where T : class;
+        Task UpdateAsync<T>(T entity) where T : class;
+        Task DeleteAsync<T>(T entity) where T : class;
+    }
+}
---
+++ Orchtech.DotNetTask/Orchtech.DotNetTask.Common/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Orchtech.DotNetTask.Common.Repository
+{
+    public class Repository<TDbContext> : IRepository where TDbContext : DbContext
+    {
+        protected TDbContext dbContext;
+
+        public Repository(TDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public async Task CreateAsync<T>(T entity) where T : class
+        {
+            this.dbContext.Set<T>().Add(entity);
+
+            _ = await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync<T>(T entity) where T : class
+        {
+            this.dbContext.Set<T>().Remove(entity);
+
+            _ = await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAll<T>() where T : class
+        {
+            return await this.dbContext.Set<T>().ToListAsync();
+        }
+
+
+        public async Task UpdateAsync<T>(T entity) where T : class
+        {
+            this.dbContext.Set<T>().Update(entity);
+
+            _ = await this.dbContext.SaveChangesAsync();
+        }
+
+        public async Task<T> Find<T>(Guid id) where T : class
+        {
+            return await this.dbContext.Set<T>().FindAsync(id);
+        }
+    }
+
+}
